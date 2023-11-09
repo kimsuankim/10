a27bbecd96a592149224648a69624abcf2ce4269
kimsuankim <144988337+kimsuankim@users.noreply.github.com>
2023-11-09 16:11:23 +0900

    6

--- main.c
+++ main.c
@@ -5,19 +5,21 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	char str[100];
-	int i;
+	char c;
 	FILE* fp;
-	//openfile
-	fp = fopen("sample.txt","w");
-	//write file
-	for (i=0; i<3; i++){
-	printf("input a word:");
-	scanf("%s", str); //str자체가 포인터 
-	fprintf(fp, "%s\n", str);
-    }
-	//close file
-	fclose(fp);
 	
+	fp = fopen("sample.txt", "r");
+	if (fp == NULL)
+	{
+		printf("failed to open\n");
+		return 0;
+	}
+	
+	while((c=fgetc(fp))!= EOF)// 파일에서 한글자 긁어온게  
+	{
+		putchar(c); // 한글자 출력 
+	}
+	
+	fclose(fp);
 	return 0;
 }
